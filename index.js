a04a1e207e667140538f840844ddf4b241c3c506
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-01-22 21:18:58 -0100

    OTP implementation

--- index.js
+++ index.js
@@ -4,18 +4,29 @@ const app = express()
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
 const jwt = require('jsonwebtoken');
+const otpGenerator = require('otp-generator')
 const token = jwt.sign({ foo: 'bar' }, 'shhhhh');
 const port = 3000
 app.use(cors())
+
+const username = '1';
+const password = '1';
+const otp = otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false });
 
 app.get('/', (req, res) => {
     res.send('Node JS Express server [JWT Token + CORS + JSON Parser]')
 })
 
 app.post('/data', jsonParser, (req, res) => {
-    const username = req.body.username
-    const password = req.body.password
-    username === '1' & password === '1' ? res.send({token: token}) : res.send('Incorrect data')
+    otp === req.body.otp ? res.send({token: token}) : res.send('Incorrect OTP. Please insert the correct data')
+})
+
+app.get('/otp', (req, res) => {
+    res.send({OTP: otp})
+})
+
+app.post('/otp', jsonParser, (req, res)=> {
+    username === req.body.username & password === req.body.password ? res.send({OTP: otp}) : res.send('Incorrect login or password. Please insert the correct data.')
 })
 
 app.listen(port, function () {
