f1b54dcb56068a039288daf70c56d2d86e23b97d
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-01-23 19:28:59 -0100

    server refactor

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -5,45 +6,31 @@ const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
 const jwt = require('jsonwebtoken');
 const otpGenerator = require('otp-generator')
-const dotEnv = require('dotenv').config()
 const token = jwt.sign({ foo: 'bar' }, 'shhhhh');
 const port = 3000
 app.use(cors())
-
-const username = '1';
-const password = '1';
 const otp = otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false });
-let requestUsername;
-let requestPassword;
-let tokenProvidedByFE;
 
 app.get('/', (req, res) => {
     res.send('Node JS Express server [JWT Token + CORS + JSON Parser]')
 })
 
 app.post('/get-otp', jsonParser, (req, res) => {
-    console.log("REQUEST", req.body)
-    requestUsername = req.body.username;
-    requestPassword = req.body.password;
+    process.env['USER_REQUEST'] = req.body.username
+    process.env['USER_PASSWORD'] = req.body.username
     res.send({OTP: otp})
 })
 
 app.post('/confirm-otp', jsonParser, (req, res)=> {
-    console.log("OTP VALUE", req.body.otp)
-    otp === req.body.otp && (username === requestUsername && password === requestPassword) ? res.send({token: token}) : res.send('Incorrect data. Please insert the correct data')
+    otp === req.body.otp && (process.env.USER_REQUEST === process.env.DB_USER && process.env.USER_PASSWORD === process.env.DB_PASSWORD) ? res.send({token: token}) : res.send('Incorrect data. Please insert the correct data')
 })
 
 app.post('/terms-and-conditions', jsonParser, (req, res)=> {
-    tokenProvidedByFE = req.header('authorization').split(' ')[1];
-    var jwtVerificedToken = jwt.verify(tokenProvidedByFE, 'shhhhh');
-    if(jwtVerificedToken) {
-        res.send({ok: 'verified'})
-    } else {
-        res.send('fail')
-    }
+    process.env['TOKEN'] = req.header('authorization').split(' ')[1];
+    const jwtToken = jwt.verify(process.env.TOKEN, 'shhhhh');
+    jwtToken ? res.send({ok: 'verified'}) : res.send('fail');
 })
 
 app.listen(port, function () {
-    console.log("process", process.env.S3_BUCKET) // remove this after you've confirmed it is working
     console.log('CORS-enabled web server listening on port 80')
 })
